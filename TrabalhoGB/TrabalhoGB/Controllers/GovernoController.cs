92fdddcc17710b7a0c74ae47f12b25f27e413c77
Roger Medeiros <rogermedeiros@outlook.com>
2017-11-04 18:04:26 -0200

    Criadas ações de Governo e ONG

---
+++ TrabalhoGB/TrabalhoGB/Controllers/GovernoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabalhoGB.Controllers
+{
+    public class GovernoController : Controller
+    {
+        // GET: Governo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
